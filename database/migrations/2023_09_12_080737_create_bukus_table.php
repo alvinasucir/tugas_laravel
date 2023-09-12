eb92a2909389bb11ba57db7cad6973bc8c7e2a35
alvinasucir <alvinarhmdn@gmail.com>
2023-09-12 15:35:24 +0700

    update

---
+++ database/migrations/2023_09_12_080737_create_bukus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bukus', function (Blueprint $table) {
+            $table->id();
+            $table->char('kode_buku', 5);
+            $table->string('judul_buku', 50);
+            $table->string('penulis_buku', 50);
+            $table->string('penerbit_buku', 50);
+            $table->char('tahun_buku', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bukus');
+    }
+};
